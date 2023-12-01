8920a17706bf5e8e404855d67b3715dbe7d84262
Jaymin Darji <jaymindarji2003@gmail.com>
2023-12-01 18:45:15 +0530

    done with the project.

---
+++ project in async await/index.js
@@ -0,0 +1,52 @@
+// setInterval(() => {
+//     console.log("jaymin darji");
+// }, 2000);
+
+
+// this function called arrow function.
+// const fnx = () => {
+//     console.log("setTimeOut function called");
+//     document.querySelector('h1').innerText = "Chai aur JavaScript";
+// }
+
+// const changeMe = setTimeout(fnx, 2000);
+
+// document.querySelector("#stop").addEventListener("click", () => {
+//     clearTimeout(changeMe);
+//     console.log("btn clicked");
+// })
+
+
+
+// project started here.
+
+// generate a random color
+const generateColor = function () {
+    const hax = "0123456789ABCDEF";
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+        color += hax[Math.floor(Math.random() * 16)]
+    }
+    console.log(color);
+    return color;
+}
+
+generateColor()
+
+let changeColor;
+
+const startChangingColor = function () {
+    changeColor = setInterval(() => {
+        document.body.style.backgroundColor = generateColor();
+    }, 1000);
+}
+
+const stopChangingColor = function () {
+    clearInterval(changeColor);
+    console.log("stoping color change");
+}
+
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
